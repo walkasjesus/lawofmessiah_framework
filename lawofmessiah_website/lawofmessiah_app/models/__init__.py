150a44c3a5006a098035ad2e650b34b446b1ae98
Merijn <merijn@privacytogether.nl>
2026-08-13 18:24:29 +0000

    Added Bible usage report

--- lawofmessiah_website/lawofmessiah_app/models/__init__.py
+++ lawofmessiah_website/lawofmessiah_app/models/__init__.py
@@ -1,5 +1,6 @@
 from .bible_books import *
 from .bible_references import BibleReferences
+from .bible_usage import *
 from .bibles import BibleTranslation, BibleTranslationMetaData
 from .file import *
 from .user_preferences import *
